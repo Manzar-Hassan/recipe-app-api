8ffa0c51fee8b815ea7d6978a8597b9ff27699fd
Manzar Hassan <manzar.h@biits.in>
2025-02-07 11:31:53 +0530

    Fix: fixes linting issue

--- app/app/test.py
+++ app/app/test.py
@@ -1,17 +1,17 @@
 # sample tests
 
+
 from django.test import SimpleTestCase
 from app import calc
 
 
 class CalcTests(SimpleTestCase):
 
-
     def test_add_numbers(self):
         result = calc.add(4, 5)
         self.assertEqual(result, 9)
-        
 
     def test_substract_numbers(self):
         result = calc.sub(10, 10)
         self.assertEqual(result, 0)
+
