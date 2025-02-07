7664cc383d74ffe2ef597138c67d67a8e60f807e
Manzar Hassan <manzar.h@biits.in>
2025-02-07 11:29:20 +0530

    solves linting issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,9 @@
 # calculator functions
 
+
 def add(x, y):
     return x + y
+
 
 def sub(x, y):
     return x - y
--- app/app/test.py
+++ app/app/test.py
@@ -3,13 +3,15 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
 
+
     def test_add_numbers(self):
-        result = calc.add(4,5)
+        result = calc.add(4, 5)
         self.assertEqual(result, 9)
+        
 
     def test_substract_numbers(self):
         result = calc.sub(10, 10)
         self.assertEqual(result, 0)
-    
