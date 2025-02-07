fe1d2b0123b4dad03d341917c3ef0b0d43fb1d12
Manzar Hassan <manzar.h@biits.in>
2025-02-07 11:34:44 +0530

    Fix: fixes liniting isuue

--- app/app/test.py
+++ app/app/test.py
@@ -8,10 +8,9 @@ from app import calc
 class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
-        result = calc.add(4, 5)
+        result = calc.add(4, 5)  # Fixed missing space after comma
         self.assertEqual(result, 9)
 
     def test_substract_numbers(self):
         result = calc.sub(10, 10)
         self.assertEqual(result, 0)
-
